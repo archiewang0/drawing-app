1ef50cd273537698251afdeafceb4f3049902b54
archie <archiewang0652060221@gmail.com>
2025-03-19 17:34:29 +0800

    update add new runtime env

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ import DrawingComponent from '../components/page/drawing/drawing'
 interface pageProps {}
 
 console.log('process.env.TEST', process.env.TEST)
+console.log('process.env.NEXT_PUBLIC_TEST', process.env.NEXT_PUBLIC_TEST)
 console.log('process.env.NODE_ENV', process.env.NODE_ENV)
 const Page: FC<pageProps> = ({}) => {
     return  <div className="container mx-auto px-4 py-8">
