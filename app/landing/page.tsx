515ed54b1dcf979c3c92fc9d09db803b56024678
Archie <76575196+archiewang0@users.noreply.github.com>
2025-05-01 17:11:37 +0800

    fix: head add preload image

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,26 +1,32 @@
-
-import { Metadata } from "next"
-import Image from "next/image";
-import Link from "next/link";
-import { ArrowBigRight } from "lucide-react";
+import {Metadata} from 'next';
+import Image from 'next/image';
+import Link from 'next/link';
+import {ArrowBigRight} from 'lucide-react';
 
 export const metadata: Metadata = {
-    title: "drawing app",
-    description: "簡單好玩的畫圖網頁",
+    title: 'drawing app',
+    description: '簡單好玩的畫圖網頁',
 };
 
 export default function ProfilePage() {
-
     return (
         <div className=" flex justify-center items-center  h-[calc(100vh-4rem)]">
-            <Image src="/drawingdog.jpeg" alt='drawing dog' width={600} height={600} />
+            <Image
+                src="/drawingdog.jpeg"
+                alt="drawing dog"
+                width={600}
+                height={600}
+            />
             <div>
                 <p>尼好 !!</p>
                 <p>我畫了一隻狗勾</p>
                 <p>現在換你了表現了。</p>
-                <Link className="flex items-center font-bold" href="/">Drawing now! <ArrowBigRight className="ml-1"/></Link>
+                <Link
+                    className="flex items-center font-bold"
+                    href="/">
+                    Drawing now! <ArrowBigRight className="ml-1" />
+                </Link>
             </div>
         </div>
-    )
+    );
 }
-
--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,22 +1,21 @@
-import Link from "next/link"
-import SignInButton from "./sign-in-button"
-import { PenIcon } from "lucide-react"
+import Link from 'next/link';
+import SignInButton from './sign-in-button';
+import {PenIcon} from 'lucide-react';
 
 export function NavBar() {
-
-
     return (
         <header className="bg-white border-b border-gray-200">
             <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-                <Link href="/" className="text-xl font-bold flex gap-2">
+                <Link
+                    href="/"
+                    className="text-xl font-bold flex gap-2">
                     <PenIcon />
                     <span>Drawing App</span>
                 </Link>
                 <div className="flex items-center space-x-4">
-                    <SignInButton/>
+                    <SignInButton />
                 </div>
             </div>
         </header>
-    )
+    );
 }
-
